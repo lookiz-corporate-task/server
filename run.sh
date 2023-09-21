648941708cde1d06648a4d8065c351641dc7e153
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-09-22 01:06:20 +0900

    fix: Move script to gradle

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "ACTION DEPLOY START"
-
-sudo fuser -k 8080/tcp || echo "No process is using port 8080"
-
-sleep 3
-
-sudo nohup java -jar ~/cicd/lookiz-0.0.1-SNAPSHOT.jar > output.log 2> nohup.err < /dev/null &
