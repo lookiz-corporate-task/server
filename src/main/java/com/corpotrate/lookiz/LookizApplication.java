3d5d4769af1df7971bfeb8aa7d0a21ddd2413767
GiwoongBang <bgw6713@naver.com>
2023-09-21 18:58:32 +0900

    first commit

---
+++ src/main/java/com/corpotrate/lookiz/LookizApplication.java
@@ -0,0 +1,13 @@
+package com.corpotrate.lookiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corpotrate/lookiz/LookizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corpotrate.lookiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
