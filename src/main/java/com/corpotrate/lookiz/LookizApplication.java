b349149298034e671f20a6ef4f5121f2035b79cb
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2023-09-23 22:40:20 +0900

    Update LookizApplication.java

--- src/main/java/com/corpotrate/lookiz/LookizApplication.java
+++ src/main/java/com/corpotrate/lookiz/LookizApplication.java
@@ -2,10 +2,13 @@ package com.corpotrate.lookiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.apache.catalina.connector.Connector;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -23,6 +26,22 @@ public class LookizApplication {
 				registry.addMapping("/**").allowedOrigins("*");
 			}
 		};
+	}
+
+	@Bean
+	public ServletWebServerFactory serverFactory() {
+		TomcatServletWebServerFactory tomcatServletWebServerFactory
+				= new TomcatServletWebServerFactory();
+		tomcatServletWebServerFactory.addAdditionalTomcatConnectors(createStandardConnector());
+
+		return tomcatServletWebServerFactory;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(8080);
+
+		return connector;
 	}
 
 }
